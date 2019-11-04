7e191e50ffe12646b2c149d00df03e73f234719c
Gabriel <gabriel.brandao@ccc.ufcg.edu.br>
2019-11-04 17:59:18 -0300

    roteiro 14

---
+++ R14/src/main/java/adt/bt/Util.java
@@ -0,0 +1,78 @@
+package adt.bt;
+
+import adt.bst.BSTNode;
+
+public class Util {
+
+
+	/**
+	 * A rotacao a esquerda em node deve subir e retornar seu filho a direita
+	 * @param node
+	 * @return
+	 */
+	public static <T extends Comparable<T>> BSTNode<T> leftRotation(BSTNode<T> node) {
+		BSTNode<T> pivot = (BSTNode<T>) node.getRight();
+		BSTNode<T> leftPivot = (BSTNode<T>) pivot.getLeft();
+		BSTNode<T> parent = (BSTNode<T>) node.getParent();
+
+		if (parent != null) {
+
+			if (parent.getLeft().equals(node))
+
+				parent.setLeft(pivot);
+
+			else
+				parent.setRight(pivot);
+
+		}
+
+		pivot.setParent(parent);
+		node.setParent(pivot);
+
+		node.setRight(leftPivot);
+		pivot.setLeft(node);
+
+		if (leftPivot != null)
+			leftPivot.setParent(node);
+
+		return pivot;
+	}
+
+	/**
+	 * A rotacao a direita em node deve subir e retornar seu filho a esquerda
+	 * @param node
+	 * @return
+	 */
+	public static <T extends Comparable<T>> BSTNode<T> rightRotation(BSTNode<T> node) {
+		BSTNode<T> pivot = (BSTNode<T>) node.getLeft();
+		BSTNode<T> rightPivot = (BSTNode<T>) pivot.getRight();
+		BSTNode<T> parent = (BSTNode<T>) node.getParent();
+
+		if(parent != null){
+
+			if (parent.getLeft().equals(node))
+				parent.setLeft(pivot);
+
+			else
+				parent.setRight(pivot);
+
+		}
+
+		pivot.setParent(parent);
+		node.setParent(pivot);
+
+		node.setLeft(rightPivot);
+		pivot.setRight(node);
+
+		if (rightPivot != null)
+			rightPivot.setParent(node);
+
+		return pivot;
+	}
+
+	public static <T extends Comparable<T>> T[] makeArrayOfComparable(int size) {
+		@SuppressWarnings("unchecked")
+		T[] array = (T[]) new Comparable[size];
+		return array;
+	}
+}
